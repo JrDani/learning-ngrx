02f3ccb7e080ef99c4824d3a82e7b43d2df3d46e
Daniel Junior <danjun48@gmail.com>
2020-05-03 01:13:38 -0300

    state fortemente tipado

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,8 +1,21 @@
-export function reducer(state, action) {
+import { Product } from '../product';
+import * as fromRoot from '../../state/app.state';
+
+export interface State extends fromRoot.State {
+    produtos: ProductState;
+}
+
+export interface ProductState {
+    showProductCode: boolean;
+    currentProduct: Product;
+    products: Product[];
+}
+
+export function reducer(state: ProductState, action): ProductState {
     switch (action.type) {
         case 'TOGGLE_PRODUCT_CODE':
-            console.log('state antigo: ' + JSON.stringify(state));
-            console.log('payload: ' + action.payload);
+            console.log('state antigo ', state);
+            console.log('payload: ', action.payload);
             return {
                 ...state,
                 showProductCode: action.payload
