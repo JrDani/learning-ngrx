fc403f0d0329d3b69e20b4268d2c5f5ef0cff88f
Daniel Junior <danjun48@gmail.com>
2020-05-12 15:32:17 -0300

    Product reducer passa a escutar action LoadSuccess

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -71,6 +71,12 @@ export function reducer(state = initialState, action: ProductActions): ProductSt
                 }
             };
 
+        case ProductActionTypes.LoadSuccess:
+            return {
+                ...state,
+                products: action.payload
+            };
+
         default:
             return state;
     }
